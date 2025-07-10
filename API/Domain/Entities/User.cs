e1127729a2d3e20d3633e1a182225488d441d442
David TA <td.ta.david@gmail.com>
2025-07-10 16:05:40 +0200

    feature: template ASP.NET Core API

---
+++ API/Domain/Entities/User.cs
@@ -0,0 +1,12 @@
+﻿namespace API.Domain.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string PasswordHash { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
